25e3ec892fdf9f585fd3c42aeb40481e3f1661f0
Julia Kupreichik <julilike@yandex.ru>
2023-04-18 19:58:59 +0300

    feat: add date format and currency select forms

--- airways/src/app/core/components/header/header.component.ts
+++ airways/src/app/core/components/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  currencies = ['EUR', 'USA', 'RUB', 'PLS'];
+  dateFormats = ['MM/dd/yyyy', 'dd/MM/yyyy', 'yyyy/dd/MM', 'yyyy/MM/dd'];
 
+  currency = this.currencies[2];
+  dateFormat = this.dateFormats[1];
 }
--- airways/src/app/shared/shared.module.ts
+++ airways/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
@@ -9,7 +9,8 @@ import { MatSelectModule } from '@angular/material/select';
     CommonModule
   ],
   exports: [
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule
   ]
 })
 export class SharedModule { }
