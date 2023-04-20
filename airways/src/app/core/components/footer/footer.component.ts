e55dd08bbffcc5521bb258aa937758b62e78df27
Julia Kupreichik <julilike@yandex.ru>
2023-04-20 19:36:25 +0300

    feat: add responsive to footer

--- airways/src/app/core/components/footer/footer.component.ts
+++ airways/src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { GITHUB_ICON } from '../../constants/svg-icons.constants';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   currentDate = Date.now();
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral('github', sanitizer.bypassSecurityTrustHtml(GITHUB_ICON));
+  }
 }
