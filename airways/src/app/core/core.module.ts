51206397ac65b8f4f0bc1ba9aea80dff5e813f70
Julia Kupreichik <julilike@yandex.ru>
2023-04-20 17:56:19 +0300

    feat: add footer

--- airways/src/app/core/core.module.ts
+++ airways/src/app/core/core.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
-
-
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ],
-  exports: [
-    HeaderComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
