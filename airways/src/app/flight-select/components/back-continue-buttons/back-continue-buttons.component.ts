6e9e10541c650f87af783ff649a60afe29884c7f
Sergey <fserega2014@yandex.ru>
2023-05-13 20:49:20 +0300

    feat: add stepper logic

--- airways/src/app/flight-select/components/back-continue-buttons/back-continue-buttons.component.ts
+++ airways/src/app/flight-select/components/back-continue-buttons/back-continue-buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { StepperService } from 'src/app/core/services/stepper.service';
 
 @Component({
@@ -7,5 +8,25 @@ import { StepperService } from 'src/app/core/services/stepper.service';
   styleUrls: ['./back-continue-buttons.component.scss'],
 })
 export class BackContinueButtonsComponent {
-  constructor(public stepperService: StepperService) {}
+  constructor(private stepperService: StepperService, private router: Router) {}
+
+  prevStep() {
+    const currentStepIndex = this.stepperService.getStepIndex();
+    const newStepIndex = currentStepIndex - 1;
+    if (newStepIndex >= 0) {
+      this.stepperService.setStepIndex(newStepIndex);
+    }
+    if (newStepIndex < 0) this.router.navigate(['/']);
+  }
+
+  nextStep() {
+    const currentStepIndex = this.stepperService.getStepIndex();
+    const newStepIndex = currentStepIndex + 1;
+    this.stepperService.setStepIndex(newStepIndex);
+  }
+
+  showButtons(): boolean {
+    const currentStepIndex = this.stepperService.getStepIndex();
+    return currentStepIndex === 2;
+  }
 }
