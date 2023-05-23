58198dd387fe3acb193b72d57290ce7aa45b07af
Sergey <fserega2014@yandex.ru>
2023-05-23 18:10:18 +0300

    feat: add summary logic

--- airways/src/app/flight-select/components/summary-total-item/summary-total-item.component.ts
+++ airways/src/app/flight-select/components/summary-total-item/summary-total-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SearchDataService } from '../../../core/services/search-data.service';
 
 @Component({
   selector: 'app-summary-total-item',
@@ -7,4 +8,6 @@ import { Component, Input } from '@angular/core';
 })
 export class SummaryTotalItemComponent {
   @Input() peopleItem: any;
+
+  constructor(public searchDataService: SearchDataService) {}
 }
--- airways/src/app/flight-select/pages/summary/summary.component.ts
+++ airways/src/app/flight-select/pages/summary/summary.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { SearchDataService } from 'src/app/core/services/search-data.service';
 import { FlightSelectService } from '../../services/flight-select.service';
+import { PassengersService } from '../../services/passengers.service';
 
+type PassengerGroup = { title: string; fare: number; tax: number; count: number };
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
@@ -10,41 +12,42 @@ import { FlightSelectService } from '../../services/flight-select.service';
 export class SummaryComponent {
   cardItems = this.flightSelectService.getSelectedCardsData(this.searchDataService.isOneWay);
 
-  peopleItems = [
-    {
-      passenger: 'adult',
-      title: 'Harry Potter',
-      checkedBag: '1 checked bag (total 23 kg) included',
-      cabinBag: '1 cabin bag + 1 personal item (max. 8 kg) included',
-      seat: '10C',
-      fare: 166,
-      tax: 91.31,
-    },
-    {
-      passenger: 'children',
-      title: 'Lili Potter',
-      checkedBag: '1 checked bag (total 23 kg) included',
-      cabinBag: '1 cabin bag + 1 personal item (max. 8 kg) included',
-      seat: '11C',
-      fare: 106,
-      tax: 90.08,
-    },
-    {
-      passenger: 'infant',
-      title: 'James Potter',
-      checkedBag: '1 checked bag (total 23 kg) included',
-      cabinBag: '1 cabin bag + 1 personal item (max. 8 kg) included',
-      fare: 88,
-      tax: 10,
-    },
-  ];
+  peopleItems = this.passengersService.passengersList;
+
+  peopleItemsGroup: PassengerGroup[] = this.peopleItems.reduce((acc, el) => {
+    const existingPassenger = acc.find((p) => p.title === el.title);
+    if (existingPassenger) {
+      if (typeof el.fare === 'number') {
+        existingPassenger.fare += el.fare;
+      }
+      if (typeof el.tax === 'number') {
+        existingPassenger.tax += el.tax;
+      }
+      existingPassenger.count += 1;
+    } else {
+      const newPassenger: PassengerGroup = {
+        title: el.title,
+        fare: typeof el.fare === 'number' ? el.fare : 0,
+        tax: typeof el.tax === 'number' ? el.tax : 0,
+        count: 1,
+      };
+      acc.push(newPassenger);
+    }
+    return acc;
+  }, [] as PassengerGroup[]);
 
   constructor(
     private flightSelectService: FlightSelectService,
-    private searchDataService: SearchDataService,
+    public searchDataService: SearchDataService,
+    private passengersService: PassengersService,
   ) {}
 
   getTotal(): number {
-    return this.peopleItems.reduce((acc, item) => acc + item.fare + item.tax, 0);
+    console.log(this.peopleItems);
+
+    return this.peopleItems.reduce(
+      (acc, item) => (item.fare && item.tax ? acc + item.fare + item.tax : acc),
+      0,
+    );
   }
 }
